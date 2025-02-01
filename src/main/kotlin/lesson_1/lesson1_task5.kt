0312f3feee15ffae59f62628fe4105962757caa9
Stekl0 <greystekl0@gmail.com>
2025-02-01 13:43:28 +0300

    KS-1-5 String.format()

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,9 +1,15 @@
 package org.stekl0.lesson_1
 
-fun main(){
-    var second = 6480
-    var minute = second/60
-    val hour = minute/60
-    minute = minute%60
-    println("0$hour:$minute:00")
+fun main() {
+    val conversion = 60
+
+    var seconds = 6480
+    var minutes = seconds / conversion
+    seconds = seconds % conversion
+    val hours = minutes / conversion
+    minutes = minutes % conversion
+
+    val result = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    println(result)
 }
