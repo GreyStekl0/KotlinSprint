7980b6a4e9a80462a7df9889dff41e21aed8e015
Stekl0 <greystekl0@gmail.com>
2025-02-02 16:24:17 +0300

    KS-1-5 .toFloat()

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.stekl0.lesson_2
+
+fun main() {
+    val schoolboy1 = 3
+    val schoolboy2 = 4
+    val schoolboy3 = 3
+    val schoolboy4 = 5
+    val boys = 4
+
+    val averageValue = (schoolboy1 + schoolboy2 + schoolboy3 + schoolboy4).toFloat() / boys
+
+    println(averageValue)
+}
