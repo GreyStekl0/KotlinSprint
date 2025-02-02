6015d1079cadd17f8abb57c5a646056fd466c297
Stekl0 <greystekl0@gmail.com>
2025-02-02 17:26:09 +0300

    KS-3-1 $variable

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package org.stekl0.lesson_3
+
+fun main() {
+    val name = "Stekl0"
+
+    var greeting = "Good morning, $name"
+    println(greeting)
+
+    greeting = "Good evening, $name"
+    println(greeting)
+}
