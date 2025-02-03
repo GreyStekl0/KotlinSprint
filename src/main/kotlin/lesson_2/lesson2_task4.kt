0c7c5be5a2a86d5a55b7bf2c7c4c0c4c3a245c24
Stekl0 <greystekl0@gmail.com>
2025-02-03 22:59:07 +0300

    KS-2-4 allPrey

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,10 +3,11 @@ package org.stekl0.lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buff = 1.2
+    val buff = 0.2
 
-    val fullCrystal = crystalOre * buff
-    val fullIron = ironOre * buff
+    val allPrey = 1 + buff
+    val fullCrystal = crystalOre * allPrey
+    val fullIron = ironOre * allPrey
 
     val buffCrystal = (fullCrystal - crystalOre).toInt()
     val buffIron = (fullIron - ironOre).toInt()
