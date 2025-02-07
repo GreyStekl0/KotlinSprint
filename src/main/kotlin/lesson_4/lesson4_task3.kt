4aab95ca39f9c6575883022bda9eb2e37f541e2c
Stekl0 <greystekl0@gmail.com>
2025-02-08 00:22:28 +0300

    KS-4-4 Refactor favorable conditions check to use consistent variable names and constants

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,19 @@
 package org.stekl0.lesson_4
 
-fun main() {
-    val isSunny = true
-    val isTentOpen = true
-    val humidity = 20
-    val season = "зима"
+const val isSunny = true
+const val isTentOpen = true
+const val humidity = 20
+const val unfavorableSeason = "зима"
 
-    val favorableConditions = isSunny && isTentOpen && humidity == 20 && season != "зима"
+fun main() {
+    val weatherToday = true
+    val tent = true
+    val humidityToday = 20
+    val currentSeeson = "зима"
+
+
+    val favorableConditions =
+        weatherToday == isSunny && tent == isTentOpen && humidityToday == humidity && currentSeeson != unfavorableSeason
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
