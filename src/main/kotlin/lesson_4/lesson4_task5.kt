4bc8d80c0e031653c559d2d05d1dba57117ebdff
Stekl0 <greystekl0@gmail.com>
2025-02-08 00:08:54 +0300

    KS-4-5 Refactor sailing conditions logic for improved readability and maintainability

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,21 +1,25 @@
 package org.stekl0.lesson_4
 
+const val maxStaff = 70
+const val minStaff = 55
+const val minBoxes = 50
+
 fun main() {
     println("Введите наличие повреждений корпуса (true/false):")
-    val hasDamage = readLine().toBoolean()
+    val hasDamage = readln().toBoolean()
     println(hasDamage)
 
     println("Введите текущий состав экипажа:")
-    val crewCount = readLine()!!.toInt()
+    val crewCount = readln().toInt()
 
     println("Введите количество ящиков с провизией на борту:")
-    val provisionBoxes = readLine()!!.toInt()
+    val provisionBoxes = readln().toInt()
 
     println("Введите благоприятность метеоусловий (true/false):")
-    val isWeatherFavorable = readLine().toBoolean()
+    val isWeatherFavorable = readln().toBoolean()
 
-    val canSetSail = (!hasDamage && crewCount in 55..70 && provisionBoxes > 50) ||
-            (hasDamage && crewCount == 70 && isWeatherFavorable && provisionBoxes >= 50)
+    val canSetSail = (!hasDamage && crewCount in minStaff..maxStaff && provisionBoxes > minBoxes) ||
+            (hasDamage && crewCount == maxStaff && isWeatherFavorable && provisionBoxes >= minBoxes)
 
     println("Может ли корабль отправиться в плавание? $canSetSail")
 }
