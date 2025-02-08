fb525475c5d060a79c4f794f41d8f60476e16274
Stekl0 <greystekl0@gmail.com>
2025-02-09 00:39:46 +0500

    KS-4-2 refactor

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,15 +1,29 @@
 package org.stekl0.lesson_4
 
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
+
 fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
 
     val weight1 = 20
     val volume1 = 80
     val weight2 = 50
     val volume2 = 100
 
-    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': ${weight1 > minWeight && weight1 <= maxWeight && volume1 < maxVolume}")
-    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': ${weight2 > minWeight && weight2 <= maxWeight && volume2 < maxVolume}")
+    val result1 = weight1 > MIN_WEIGHT && weight1 <= MAX_WEIGHT && volume1 < MAX_VOLUME
+    val result2 = weight2 > MIN_WEIGHT && weight2 <= MAX_WEIGHT && volume2 < MAX_VOLUME
+
+    println(
+        "Груз с весом $weight1 кг " +
+                "и объемом $volume1 л " +
+                "соответствует категории 'Average': " +
+                "$result1"
+    )
+    println(
+        "Груз с весом $weight2 кг " +
+                "и объемом $volume2 л " +
+                "соответствует категории 'Average': " +
+                "$result2"
+    )
 }
