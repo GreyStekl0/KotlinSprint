50340ef7a4f9898b4eba2ab666a502593c43e998
Stekl0 <greystekl0@gmail.com>
2025-02-09 20:22:47 +0500

    Add Detekt plugin configuration for code quality checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     kotlin("jvm") version "2.1.0"
+    id("io.gitlab.arturbosch.detekt") version("1.23.7")
 }
 
 group = "org.stekl0"
@@ -7,6 +10,21 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+detekt {
+    buildUponDefaultConfig = true // Предварительная настройка с использованием конфигурации по умолчанию
+    allRules = false // Активировать все доступные (включая нестабильные) правила
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        html.required.set(true) // Генерация HTML-отчета
+        xml.required.set(false) // Генерация XML-отчета
+        sarif.required.set(true) // Генерация SARIF-отчета
+        md.required.set(true) // Генерация Markdown-отчета
+        txt.required.set(false) // Генерация TXT-отчета
+    }
 }
 
 dependencies {
@@ -17,5 +35,5 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(21)
 }
